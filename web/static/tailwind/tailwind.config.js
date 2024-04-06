2984cc143c548b1c4707414b3d6a5bf6eee7cbe9
gosmach1ne <1234567yhb@gmail.com>
2024-04-06 12:43:53 +0300

    htmx + templ upd

---
+++ web/static/tailwind/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    "../../templates/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
+  ],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class"
+};
